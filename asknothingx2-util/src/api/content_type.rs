fd60617446657aed8b9b750057ce58002829e914
m3id <m3id.eprac@gmail.com>
2024-10-21 14:10:39 +0900

    Add: asknothingx2-util fn CONTENT_TYPE_TEXT

--- asknothingx2-util/src/api/content_type.rs
+++ asknothingx2-util/src/api/content_type.rs
@@ -10,9 +10,14 @@ pub fn CONTENT_TYPE_FORMENCODED() -> HeaderValue {
     HeaderValue::from_str("application/x-www-form-urlencoded").unwrap()
 }
 
+#[allow(non_snake_case)]
+pub fn CONTENT_TYPE_TEXT() -> HeaderValue {
+    HeaderValue::from_str("text/plain").unwrap()
+}
+
 #[cfg(test)]
 mod tqpe_tests {
-    use crate::api::{CONTENT_TYPE_FORMENCODED, CONTENT_TYPE_JSON};
+    use crate::api::{CONTENT_TYPE_FORMENCODED, CONTENT_TYPE_JSON, CONTENT_TYPE_TEXT};
 
     #[test]
     fn content_type_json() {
@@ -25,5 +30,10 @@ mod tqpe_tests {
             CONTENT_TYPE_FORMENCODED(),
             "application/x-www-form-urlencoded"
         );
+    }
+
+    #[test]
+    fn content_type_text() {
+        assert_eq!(CONTENT_TYPE_TEXT(), "text/plain");
     }
 }
