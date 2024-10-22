3a921be8dd39b3d6e75d4add5495292b00a3d52c
m3id <m3id.eprac@gmail.com>
2024-10-23 04:41:41 +0900

    Test: asknothingx2-util header_builder

--- asknothingx2-util/src/api/header_builder.rs
+++ asknothingx2-util/src/api/header_builder.rs
@@ -42,3 +42,62 @@ impl HeaderBuilder {
         self._inner
     }
 }
+
+#[cfg(test)]
+mod test {
+    use http::HeaderValue;
+
+    use super::HeaderBuilder;
+
+    #[test]
+    fn header_builder() {
+        let headers = HeaderBuilder::new().build();
+        assert_eq!(0, headers.len());
+
+        let headers = HeaderBuilder::new().content_type_formencoded().build();
+        assert_eq!(1, headers.len());
+        let content_type = headers.get("content-type");
+        assert!(content_type.is_some());
+        let content_type = headers.get("CONTENT-TYPE");
+        assert!(content_type.is_some());
+
+        assert_eq!(
+            Some(HeaderValue::from_str("application/x-www-form-urlencoded").unwrap()),
+            content_type.cloned()
+        );
+        assert_ne!(
+            Some(HeaderValue::from_str("APPLICATION/X-WWW-FORM-URLENCODED").unwrap()),
+            content_type.cloned()
+        );
+        let headers = HeaderBuilder::new()
+            .content_type_formencoded()
+            .accept_json()
+            .build();
+        assert_eq!(2, headers.len());
+
+        let accept = headers.get("accept");
+        assert!(accept.is_some());
+        let accept = headers.get("ACCEPT");
+        assert!(accept.is_some());
+        assert_eq!(
+            Some(HeaderValue::from_str("application/json").unwrap()),
+            accept.cloned()
+        );
+
+        let headers = HeaderBuilder::new()
+            .content_type_formencoded()
+            .accept_json()
+            .authorization("Oauth", "credentials")
+            .build();
+        assert_eq!(3, headers.len());
+        let authorization = headers.get("authorization");
+        assert!(authorization.is_some());
+        let authorization = headers.get("AUTHORIZATION");
+        assert!(authorization.is_some());
+
+        assert_eq!(
+            Some(HeaderValue::from_str("Oauth credentials").unwrap()),
+            authorization.cloned()
+        );
+    }
+}
