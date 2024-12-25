f8140de417dc3dfd1551ca441d201b78640c13ba
m3id <m3id.eprac@gmail.com>
2024-12-25 15:21:17 +0900

    refactor(subscription): replace manual deserialization with macro

--- asknothingx2-eventsub/src/twitch/payload/subscription.rs
+++ asknothingx2-eventsub/src/twitch/payload/subscription.rs
@@ -1,7 +1,9 @@
 use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
-use crate::twitch::{reference::transport::Transport, subscription_type::SubscriptionTypes};
+use crate::twitch::{
+    reference::transport::Transport, subscription_types::types::SubscriptionTypes,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SubscriptionPayload<Condition> {
@@ -27,59 +29,12 @@ pub struct Subscription<Condition> {
     pub created_at: DateTime<FixedOffset>,
 }
 
-impl<'de, Condition: Deserialize<'de>> Deserialize<'de> for Subscription<Condition> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        #[derive(Deserialize)]
-        pub struct Helper<Condition> {
-            id: String,
-            status: String,
-            #[serde(rename = "type")]
-            kind: SubscriptionTypes,
-            version: String,
-            cost: u64,
-            condition: Condition,
-            transport: Transport,
-            created_at: DateTime<FixedOffset>,
-        }
-        let helper = Helper::deserialize(deserializer)?;
-
-        let kind = match helper.kind {
-            kind @ SubscriptionTypes::AutomodMessageHold => {
-                if helper.version == "2" {
-                    SubscriptionTypes::AutomodMessageHoldV2
-                } else {
-                    kind
-                }
-            }
-            kind @ SubscriptionTypes::AutomodMessageUpdate => {
-                if helper.version == "2" {
-                    SubscriptionTypes::AutomodMessageUpdateV2
-                } else {
-                    kind
-                }
-            }
-            kind @ SubscriptionTypes::ChannelModerate => {
-                if helper.version == "2" {
-                    SubscriptionTypes::ChannelModerateV2
-                } else {
-                    kind
-                }
-            }
-            _ => helper.kind,
-        };
-
-        Ok(Subscription {
-            id: helper.id,
-            status: helper.status,
-            kind,
-            version: helper.version,
-            cost: helper.cost,
-            condition: helper.condition,
-            transport: helper.transport,
-            created_at: helper.created_at,
-        })
+impl_de_with_subscription_type_must_have_veasion_and_condition!(
+    Subscription {
+        id: String,
+        status: String,
+        cost: u64,
+        transport: Transport,
+        created_at: DateTime<FixedOffset>
     }
-}
+);
