f870431d203715c6b36ef283673361640de33f18
m3id <m3id.eprac@gmail.com>
2024-12-28 21:18:17 +0900

    style:

--- asknothingx2-eventsub/tests/main.rs
+++ asknothingx2-eventsub/tests/main.rs
@@ -3,11 +3,3 @@ mod util;
 
 mod subscription;
 mod websocket_message;
-refactor(eventsub): reorganize structure
-
-- Type alias EventPayload, SingePayload
-- Reorganize feature flags
-- Rename SubscriptionTypes to SubscriptionType
-- Add new_request macro warpping SubscriptionRequest
-- Add response_payload macro warpping 
-- Add fn_expected_request, fn_expected_payload macro for test
