52bca89a83983ee261d5cb81fad8cdd9af4dad9e
m3id <m3id.eprac@gmail.com>
2025-01-10 13:05:59 +0900

    feat(util/api): expose resposne body empty handlers

--- asknothingx2-util/src/api/mod.rs
+++ asknothingx2-util/src/api/mod.rs
@@ -5,7 +5,9 @@ mod error;
 mod header_builder;
 
 pub use api_request::{api_request, form_urlencoded_serialize, APIRequest};
-pub use api_response::{APIError, APIResponse};
+pub use api_response::{
+    APIError, APIResponse, EmptyArrayResponse, EmptyObjectResponse, EmptyStringResponse,
+};
 pub use content_type::ContentType;
 pub use error::JsonError;
 pub use header_builder::HeaderBuilder;
